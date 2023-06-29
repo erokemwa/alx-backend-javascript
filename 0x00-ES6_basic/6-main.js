828a231262ce21ca60701896096290cbf42fa1ec
erokemwa <erokemwa@yahoo.com>
2023-06-29 01:34:29 -0700

    Take advantage of template literals

--- 0x00-ES6_basic/6-main.js
+++ 0x00-ES6_basic/6-main.js
@@ -1,10 +1,3 @@
-import handleProfileSignup from './6-final-user';
+import getSanFranciscoDescription from './6-string-interpolation.js';
 
-console.log(handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg'));
-
-async function asyncReturn() {
-  const ret = await handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg');
-  console.log(ret);
-}
-
-asyncReturn();
+console.log(getSanFranciscoDescription());
